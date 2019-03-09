9767e2dbc68538dde9027a9e61f9f629cc2caa20
irabi <irabikassim01@gmail.com>
2019-03-09 11:42:06 +0300

    add columns in appartment table

--- apartment/models.py
+++ apartment/models.py
@@ -3,10 +3,18 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Appartments(models.Model):
+    kitchen_room = (
+        ('True','True'),('False','False')
+    )
     title = models.CharField(max_length = 200)
     price = models.PositiveIntegerField()
     slug = models.SlugField(max_length = 200, default = "irabi")
-    # image = models.FileField(default='settings.MEDIA_ROOT/logos/anonymous.jpg')
+    area_range = models.PositiveIntegerField(default = 1200)
+    Bedrooms = models.PositiveIntegerField(default = 2)
+    Bathroom = models.PositiveIntegerField(default = 2)
+    kitchen = models.CharField(max_length = 10, default = "True", choices = kitchen_room)
+    image = models.FileField(default='settings.MEDIA_ROOT/logos/anonymous.jpg')
+
     # def slug(self):
     #     return slugify(self.title)
     class Meta:
