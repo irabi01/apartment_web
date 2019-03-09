755e93f3b41e2595f319d7bc1a78b00a72645ec0
irabi <irabikassim01@gmail.com>
2019-03-09 12:15:17 +0300

    add paragraph to the details page

--- apartment/models.py
+++ apartment/models.py
@@ -6,10 +6,14 @@ class Appartments(models.Model):
     kitchen_room = (
         ('True','True'),('False','False')
     )
+    roomtype = (
+        ('Furnished','Furnished'),('Unfurnished','Unfurnished')
+    )
     title = models.CharField(max_length = 200)
     price = models.PositiveIntegerField()
     slug = models.SlugField(max_length = 200, default = "irabi")
     area_range = models.PositiveIntegerField(default = 1200)
+    room_type = models.CharField(max_length = 50, default = "Furnished", choices = roomtype)
     Bedrooms = models.PositiveIntegerField(default = 2)
     Bathroom = models.PositiveIntegerField(default = 2)
     kitchen = models.CharField(max_length = 10, default = "True", choices = kitchen_room)
